1b60dca5d75de4ddf70da465a4a752103aa1eec1
Amy Tian <amy.tian789@gmail.com>
2017-03-13 01:16:17 -0400

    Fixed bug that allowed self-directing loops in simulation

--- main.R
+++ main.R
@@ -29,7 +29,8 @@ randomdraw <- function(n, prob){
 
 # Create base graph
 set.seed(10)
-bg <- igraph::sample_gnm(20, 50)
+ss <- 50 # desired sample size (# of edges)
+bg <- igraph::sample_gnm(20, ss)
 bg_e <- igraph::as_edgelist(bg)
 
 # Setting parameters
@@ -41,29 +42,29 @@ msg <- rep(0,1000)
 for (i in 1:1000) {
   set.seed(i)
   
-  # g1: randomly swap 20 edges. Weight of each node is proportional to its degree
+  # g1: randomly swap 20% edges. Weight of each node is proportional to its degree
   g1 <- bg
-  for (j in 1:20) {
+  for (j in 1:(ss/5)) {
     idx <- sample(igraph::as_edgelist(g1),1)
     g1 <- igraph::delete_edges(g1,idx)
     prob <- igraph::degree(g1) / sum(igraph::degree(g1))
     nva <- sample(seq(1,length(prob),1),1)
     nvb <- randomdraw(1, prob)
     # make sure edges are not repeated
-    while (g1[nva,nvb] == 1 & nva != nvb) nvb <- randomdraw(1,prob)
+    while (g1[nva,nvb] == 1 | nva == nvb) nvb <- randomdraw(1,prob)
     g1 <- igraph::add_edges(g1, c(nva,nvb))
   }
   
-  # g2: randomly swap 100 edges. Weight of each node is proportional to its degree
+  # g2: randomly swap 100% edges. Weight of each node is proportional to its degree
   g2 <- bg
-  for (j in 1:100) {
+  for (j in 1:ss) {
     idx <- sample(igraph::as_edgelist(g2),1)
     g2 <- igraph::delete_edges(g2,idx)
     prob <- igraph::degree(g2) / sum(igraph::degree(g2))
     nva <- sample(seq(1,length(prob),1),1)
     nvb <- randomdraw(1, prob)
     # make sure edges are not repeated
-    while (g2[nva,nvb] == 1 & nva != nvb) nvb <- randomdraw(1,prob)
+    while (g2[nva,nvb] == 1 | nva == nvb) nvb <- randomdraw(1,prob)
     g2 <- igraph::add_edges(g2, c(nva,nvb))
   }
   
